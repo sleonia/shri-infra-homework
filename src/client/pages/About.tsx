01f14ba789dd6910c7e58d07b3ca4860e0c81deb
Igor Barbashov <bis200705@gmail.com>
2024-07-17 01:46:02 +0300

    feat: fix test

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: Igor
+            Author: Igor Barbashov
           </p>
         </div>
       </div>
